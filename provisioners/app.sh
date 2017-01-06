917e94b6829731546b2259769ec66b88fcaf158d
Horacio Granillo <granillo.h@gmail.com>
2017-01-06 17:18:32 -0300

    Added python2 support

--- provisioners/app.sh
+++ provisioners/app.sh
@@ -11,21 +11,45 @@ addgroup -g 2000 err && adduser -u 2000 -S -G err err
 mkdir -p /err/.ssh/
 chown err:err /err/.ssh/
 
+# Setup Errbot configuration folders
 mkdir -p /err/data /err/virtualenv
 chown err:err /err/data /err/virtualenv
 
-gosu err python3 -m venv /err/virtualenv
+# Install dependencies
+if [[ $ERR_PYTHON_VERSION == "2" ]]; then
+	gosu err virtualenv --python /usr/bin/python /err/virtualenv
+elif [[ $ERR_PYTHON_VERSION == "3" ]]; then
+	gosu err python3 -m venv /err/virtualenv
+else
+	echo "Unsupported Python version requested through ERR_PYTHON_VERSION"
+	exit 1
+fi
 
 # Install Err itself
-gosu err /err/virtualenv/bin/pip install $ERR_PACKAGE
+if [[ $ERR_PYTHON_VERSION == "2" ]]; then
+	gosu err /err/virtualenv/bin/pip install errbot==4.2.2
+elif [[ $ERR_PYTHON_VERSION == "3" ]]; then
+	gosu err /err/virtualenv/bin/pip install $ERR_PACKAGE
+else
+	echo "Unsupported Python version requested through ERR_PYTHON_VERSION"
+	exit 1
+fi
 # XMPP back-end dependencies
 gosu err /err/virtualenv/bin/pip install sleekxmpp pyasn1 pyasn1-modules
 # IRC back-end dependencies
 gosu err /err/virtualenv/bin/pip install irc
 # HypChat back-end dependencies
 gosu err /err/virtualenv/bin/pip install hypchat
-# Slack back-end dependencies. Note: Installing from master because PyPI
-# release is broken at this time.
-gosu err /err/virtualenv/bin/pip install https://github.com/slackhq/python-slackclient/archive/master.zip
+# Slack back-end dependencies.
+# Python2 install should use slackclient 1.0.2
+# https://github.com/errbotio/errbot/pull/936
+if [[ $ERR_PYTHON_VERSION == "2" ]]; then
+	gosu err /err/virtualenv/bin/pip install slackclient==1.0.2
+elif [[ $ERR_PYTHON_VERSION == "3" ]]; then
+	gosu err /err/virtualenv/bin/pip install slackclient
+else
+	echo "Unsupported Python version requested through ERR_PYTHON_VERSION"
+	exit 1
+fi
 # Telegram back-end dependencies
 gosu err /err/virtualenv/bin/pip install python-telegram-bot
--- provisioners/base.sh
+++ provisioners/base.sh
@@ -11,24 +11,31 @@ echo DEBIAN_FRONTEND="noninteractive" >> /etc/environment
 #locale-gen en_US.UTF-8
 . /etc/environment
 
-PYTHON_PACKAGES="python3 python3-dev"
+#PYTHON_PACKAGES="python3 python3-dev"
 
 # Do a dist-upgrade because docker has not always updated their
 # images in a timely manner after security updates were released.
 apk upgrade --no-cache
 
 # Python and related packages itself (some dev libs for building C extensions)
-$APTINSTALL $PYTHON_PACKAGES build-base openssl-dev libffi-dev
+$APTINSTALL build-base openssl-dev libffi-dev
+
+if [[ $ERR_PYTHON_VERSION == "2" ]]; then
+	PYTHON_PACKAGES="python python-dev py-virtualenv gdbm py-gdbm"
+elif [[ $ERR_PYTHON_VERSION == "3" ]]; then
+	PYTHON_PACKAGES="python3 python3-dev"
+else
+	echo "Unsupported Python version requested through ERR_PYTHON_VERSION"
+	exit 1
+fi
+
+$APTINSTALL $PYTHON_PACKAGES
 
 # TLS certs and sudo are needed, curl and vim are tremendously useful when entering
 # a container for debugging (while barely increasing image size)
 # Git and openssh-client are needed to install nearly all plugins
 $APTINSTALL ca-certificates sudo curl git openssh-client
-#ln -s /usr/bin/vi /usr/bin/vim
 
 # Automatically add unknown host keys, users have no way to answer yes
 # when using 'ask'.
 echo '    StrictHostKeyChecking no' >> /etc/ssh/ssh_config
-
-# apt-get autoremove
-# rm -rf /var/lib/apt/lists/* /var/cache/apt/archives/*.deb
